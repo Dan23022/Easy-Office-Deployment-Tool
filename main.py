8952ec216f2e859c5b6267331f9a71a8bb3f625a
Dan23022 <95471799+Dan23022@users.noreply.github.com>
2023-02-03 16:04:01 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+from PyQt5.uic.properties import QtGui, QtCore
+import sys
+import re
+import xml.etree.ElementTree as ET
+import os
+
+
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+
+        self.submit_button.clicked.connect(self.submit)
+
+
+    def submit(self):
+        desired_channel = self.channel_dropdown.currentText()
+        bit = self.bit_dropdown.currentText()
+        office_build = self.office_build_edit_area.text()
+        enable_updates = self.enable_updates_dropdown.currentText()
+
+        parser = ET.XMLParser(target=ET.TreeBuilder(insert_comments=True))
+        tree = ET.parse("config.xml", parser=parser)
+        root = tree.getroot()
+
+        for elememnt in root.iter():
+            if elememnt.tag == "Add":
+                elememnt.attrib["Channel"] = f"{desired_channel}"
+
+        for elememnt in root.iter():
+            if elememnt.tag == "Add":
+                elememnt.attrib["Version"] = f"{office_build}"
+
+        for elememnt in root.iter():
+            if elememnt.tag == "Add":
+                elememnt.attrib["OfficeClientEdition"] = f"{bit}"
+
+        comments = [element for element in root.iter() if element.tag == ET.Comment]
+
+        for comment in comments:
+            if "Enabled" in comment.text:
+                comment.text = f"  <Updates Enabled=\"{enable_updates}\" Channel=\"{desired_channel}\" />  "
+
+
+        tree.write("config.xml")
+        os.system('setup /configure config.xml')
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = MyWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
