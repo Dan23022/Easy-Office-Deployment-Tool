248de644f2dd3c42ebba5a3ba9e6843e02470335
Dan23022 <95471799+Dan23022@users.noreply.github.com>
2023-02-21 16:26:59 +0000

    Added error handling to the application

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ from PyQt5.uic.properties import QtGui, QtCore
 import sys
 import xml.etree.ElementTree as ET
 import os
-import string
+import regex as re
 
 
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
-
         self.submit_button.clicked.connect(self.submit)
 
+    def error_message_fun(self):
+        error_message = QMessageBox()
+        error_message.setIcon(QMessageBox.Warning)
+        error_message.setWindowTitle('Invalid Input Detected')
+        error_message.setText("Invalid input detected in build edit area")
+        error_message.exec_()
 
     def submit(self):
         desired_channel = self.channel_dropdown.currentText()
@@ -26,15 +31,30 @@ class MyWindow(QtWidgets.QMainWindow):
         tree = ET.parse("config.xml", parser=parser)
         root = tree.getroot()
 
-
-
         for elememnt in root.iter():
             if elememnt.tag == "Add":
                 elememnt.attrib["Channel"] = f"{desired_channel}"
 
         for elememnt in root.iter():
             if elememnt.tag == "Add":
-                elememnt.attrib["Version"] = f"{office_build}"
+
+                if office_build == "":
+                    elememnt.attrib["Version"] = f"{office_build}"
+
+                elif re.search('[a-zA-Z]', office_build) and not re.search('[.]', office_build):
+                    self.error_message_fun()
+                    return
+
+                elif office_build.isalnum() and office_build.isascii():
+                    self.error_message_fun()
+                    return
+
+                elif re.search('[.]', office_build) and not re.search('[a-zA-Z]', office_build):
+                    elememnt.attrib["Version"] = f"16.0.{office_build}"
+
+                else:
+                    self.error_message_fun()
+                    return
 
         for elememnt in root.iter():
             if elememnt.tag == "Add":
@@ -46,12 +66,8 @@ class MyWindow(QtWidgets.QMainWindow):
             if "Enabled" in comment.text:
                 comment.text = f"  <Updates Enabled=\"{enable_updates}\" Channel=\"{desired_channel}\" />  "
 
-
         tree.write("config.xml")
         os.system('setup /configure config.xml')
-
-
-
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
